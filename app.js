fd210f15f102886d35395e0d9809b7c0438be9e4
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2024-11-18 11:51:02 -0300

    Add linting

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-undef */
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
-app.get("/version", (req, res) => {
-  res.send("1");
-});
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
